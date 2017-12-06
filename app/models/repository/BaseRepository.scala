3c35e1831d5febb2f1d28148e5695d01553c1621
merfemor <efremov.roman98@gmail.com>
2017-12-06 03:32:38 +0300

    added TODO

--- app/models/repository/BaseRepository.scala
+++ app/models/repository/BaseRepository.scala
@@ -5,4 +5,6 @@ import play.db.ebean.EbeanConfig
 
 protected abstract class BaseRepository(protected val ebeanConfig: EbeanConfig) {
   protected val ebeanServer: EbeanServer = Ebean.getServer(ebeanConfig.defaultServer())
+
+  // TODO: change return types of all repositories from Java to Scala types
 }
