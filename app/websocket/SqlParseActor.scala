30b2c9705de19a1a8a93220935d37ed179e00098
merfemor <efremov.roman98@gmail.com>
2018-03-11 19:59:20 +0300

    Implement Select statement executing

--- app/websocket/SqlParseActor.scala
+++ app/websocket/SqlParseActor.scala
@@ -1,6 +1,7 @@
 package websocket
 
-import java.sql.{DriverManager, ResultSet}
+import java.sql
+import java.sql.DriverManager
 
 import akka.actor.{Actor, ActorRef}
 import models.entity.SqlRight
@@ -11,7 +12,8 @@ import net.sf.jsqlparser.statement.insert.Insert
 import net.sf.jsqlparser.statement.select.Select
 import net.sf.jsqlparser.statement.update.Update
 import play.api.Logger
-import websocket.event.{AuthInfo, AuthorizedSqlQueryEvent}
+import play.api.libs.json.Json
+import websocket.event.{AuthInfo, AuthorizedSqlQueryEvent, SelectResponse}
 
 import scala.util.Try
 
@@ -29,46 +31,50 @@ class SqlParseActor extends Actor {
     Try(CCJSqlParserUtil.parse(query)).fold(e => {
       actorRef ! s"Failed to parse SQL query: ${e.getMessage}"
     }, st => {
-      checkRights(st, actorRef, authInfo)
-      executeStatement(query, st, actorRef, authInfo)
+      checkRightsAndExecute(query, st, actorRef, authInfo)
     })
   }
 
-  private def checkRights(st: Statement, actorRef: ActorRef, authInfo: AuthInfo): Unit = {
+  private def checkRightsAndExecute(query: String, st: Statement, actorRef: ActorRef, authInfo: AuthInfo): Unit = {
     st match {
       case s: Select =>
         if (!authInfo.rights.exists(SqlRight.isIncludes(SqlRight.READ_ONLY, _))) {
           actorRef ! "Unable to execute SQL query: no right to read"
           return
         }
+        executeSelectQuery(query, authInfo).fold(
+          e => actorRef ! s"Failed to execute SQL query: ${e.getMessage}",
+          resp => actorRef ! Json.toJson(resp)
+        )
       case s: Insert | Delete | Update =>
         if (!authInfo.rights.exists(SqlRight.isIncludes(SqlRight.DML, _))) {
           actorRef ! "Unable to execute SQL query: no right to modify"
           return
         }
+        ???
       case _ => actorRef ! "Unsupported SQL query type"
     }
   }
 
-  private def executeStatement(query: String, st: Statement, actorRef: ActorRef, authInfo: AuthInfo): Unit = {
-    st match {
-      case s: Select =>
-        executeSelectQuery(query, authInfo).fold(e => {
-          actorRef ! s"Failed to execute SQL query: ${e.getMessage}"
-        }, rs => {
-          ???
-        })
-      case _ =>
-        ???
+  private def executeSelectQuery(query: String, authInfo: AuthInfo): Try[SelectResponse] = {
+    execInStatement(authInfo.dbConnection.url) { statement =>
+      val rs = statement.executeQuery(query)
+      val columnsRange = Range(0, rs.getMetaData.getColumnCount)
+      val objs: Seq[Seq[Object]] = Seq[Seq[Object]]()
+      while (rs.next()) {
+        objs +: columnsRange.map(rs.getObject)
+      }
+      SelectResponse(objs)
     }
   }
 
-  private def executeSelectQuery(query: String, authInfo: AuthInfo): Try[ResultSet] = {
-    Try {
-      val connection = DriverManager.getConnection(authInfo.dbConnection.url)
-      val statement = connection.createStatement()
-      statement.executeQuery(query)
-    }
+  private def execInStatement[A](url: String)(fn: sql.Statement => A): Try[A] = Try {
+    val con = DriverManager.getConnection(url)
+    val st = con.createStatement()
+    val res = fn(st)
+    st.close()
+    con.close()
+    res
   }
 
   private def logmsg = s"${WebSocketActor.getClass.getName}:${self.path}: " + _
---
+++ app/websocket/event/Response.scala
@@ -0,0 +1,3 @@
+package websocket.event
+
+case class SelectResponse(objs: Seq[Seq[Object]])
